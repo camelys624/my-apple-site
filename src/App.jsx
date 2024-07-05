a1e621c2468fd72311f20a3699ef70662da9b4d3
camel <camel_yangz@163.com>
2024-07-05 23:34:10 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import "./App.css";
+import Model from "./components/Model";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Model.jsx
@@ -0,0 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Model = () => {
+  useGSAP(() => {
+    gsap.to("#heading", { y: 0, opacity: 1 });
+  }, []);
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a close look.
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
